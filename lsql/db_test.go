7b36cfc0fe201bdb446c5289edcebc6314150525
Rangel Reale <rangelspam@gmail.com>
2024-06-06 18:05:15 -0300

    test option

--- lsql/db_test.go
+++ lsql/db_test.go
@@ -48,3 +48,45 @@ func TestNewDB(t *testing.T) {
 
 	assert.NilError(t, rows.Err())
 }
+
+func TestNewDBQueryHandler(t *testing.T) {
+	ctx := context.Background()
+
+	db, dbMock, err := sqlmock.New()
+	assert.NilError(t, err)
+	defer db.Close()
+
+	dbMock.ExpectQuery(`SELECT (.+) FROM film WHERE length > (.+) LIMIT (.+)`).
+		WithArgs(90, 10).
+		WillReturnRows(sqlmock.
+			NewRows([]string{"film_id", "title", "length"}).
+			AddRow(1, "Test Film", 90))
+
+	ddb := NewDB(db, WithQueryHandler(sq.NewHandler(
+		sq.WithDefaultBuildOptions(
+			sq.WithWriterOptions(sq.WithUseNewLine(false)),
+		),
+	)))
+
+	query := psql.Select(
+		sm.Columns("film_id", "title", "length"),
+		sm.From("film"),
+		sm.WhereC("length > ?", sq.NamedArg("length")),
+		sm.Limit(10),
+	)
+
+	rows, err := ddb.Query(ctx, query, map[string]any{
+		"length": 90,
+	})
+	assert.NilError(t, err)
+	defer rows.Close()
+
+	for rows.Next() {
+		var film_id, length int
+		var title string
+		err = rows.Scan(&film_id, &title, &length)
+		assert.NilError(t, err)
+	}
+
+	assert.NilError(t, rows.Err())
+}
