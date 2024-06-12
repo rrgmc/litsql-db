9897f757c73f63a0ee02e15f0dc4faf254941fc1
Rangel Reale <rangelspam@gmail.com>
2024-06-12 05:30:51 -0300

    separate T directory

---
+++ lpgx/lpgxt/stmt.go
@@ -0,0 +1,68 @@
+package lpgxt
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/rrgmc/litsql/sq"
+)
+
+// Stmt wraps a PGXQuerier with a statement description from a Prepare call.
+type Stmt[T PGXQuerier] struct {
+	stmt         T
+	desc         *pgconn.StatementDescription
+	args         []any
+	queryHandler sq.Handler
+}
+
+// NewStmt wraps a PGXQuerier with a statement description from a Prepare call.
+func NewStmt[T PGXQuerier](querier T, desc *pgconn.StatementDescription, args []any, options ...Option) *Stmt[T] {
+	var optns dbOptions
+	for _, opt := range options {
+		opt(&optns)
+	}
+
+	if optns.queryHandler == nil {
+		optns.queryHandler = sq.NewHandler()
+	}
+
+	return &Stmt[T]{
+		stmt:         querier,
+		desc:         desc,
+		args:         args,
+		queryHandler: optns.queryHandler,
+	}
+}
+
+func (d *Stmt[T]) Handler() T {
+	return d.stmt
+}
+
+func (d *Stmt[T]) Query(ctx context.Context, params any) (pgx.Rows, error) {
+	args, err := d.buildArgs(params)
+	if err != nil {
+		return nil, err
+	}
+	return d.stmt.Query(ctx, d.desc.Name, args...)
+}
+
+func (d *Stmt[T]) QueryRow(ctx context.Context, params any) (pgx.Row, error) {
+	args, err := d.buildArgs(params)
+	if err != nil {
+		return nil, err
+	}
+	return d.stmt.QueryRow(ctx, d.desc.Name, args...), nil
+}
+
+func (d *Stmt[T]) Exec(ctx context.Context, params any) (pgconn.CommandTag, error) {
+	args, err := d.buildArgs(params)
+	if err != nil {
+		return pgconn.CommandTag{}, err
+	}
+	return d.stmt.Exec(ctx, d.desc.Name, args...)
+}
+
+func (d *Stmt[T]) buildArgs(params any) ([]any, error) {
+	return d.queryHandler.ParseArgs(d.args, params)
+}
--- lpgx/stmt.go
+++ lpgx/stmt.go
@@ -1,68 +1,19 @@
 package lpgx
 
 import (
-	"context"
-
 	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
-	"github.com/rrgmc/litsql/sq"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rrgmc/litsql-db/lpgx/lpgxt"
 )
 
-// Stmt wraps a PGXQuerier with a statement description from a Prepare call.
-type Stmt[T PGXQuerier] struct {
-	stmt         T
-	desc         *pgconn.StatementDescription
-	args         []any
-	queryHandler sq.Handler
-}
+type StmtConn = lpgxt.Stmt[*pgx.Conn]
 
-// NewStmt wraps a PGXQuerier with a statement description from a Prepare call.
-func NewStmt[T PGXQuerier](querier T, desc *pgconn.StatementDescription, args []any, options ...Option) *Stmt[T] {
-	var optns dbOptions
-	for _, opt := range options {
-		opt(&optns)
-	}
+type StmtTx = lpgxt.Stmt[pgx.Tx]
 
-	if optns.queryHandler == nil {
-		optns.queryHandler = sq.NewHandler()
-	}
+type StmtPoolTx = lpgxt.Stmt[*pgxpool.Tx]
 
-	return &Stmt[T]{
-		stmt:         querier,
-		desc:         desc,
-		args:         args,
-		queryHandler: optns.queryHandler,
-	}
-}
-
-func (d *Stmt[T]) Handler() T {
-	return d.stmt
-}
-
-func (d *Stmt[T]) Query(ctx context.Context, params any) (pgx.Rows, error) {
-	args, err := d.buildArgs(params)
-	if err != nil {
-		return nil, err
-	}
-	return d.stmt.Query(ctx, d.desc.Name, args...)
-}
-
-func (d *Stmt[T]) QueryRow(ctx context.Context, params any) (pgx.Row, error) {
-	args, err := d.buildArgs(params)
-	if err != nil {
-		return nil, err
-	}
-	return d.stmt.QueryRow(ctx, d.desc.Name, args...), nil
-}
-
-func (d *Stmt[T]) Exec(ctx context.Context, params any) (pgconn.CommandTag, error) {
-	args, err := d.buildArgs(params)
-	if err != nil {
-		return pgconn.CommandTag{}, err
-	}
-	return d.stmt.Exec(ctx, d.desc.Name, args...)
-}
-
-func (d *Stmt[T]) buildArgs(params any) ([]any, error) {
-	return d.queryHandler.ParseArgs(d.args, params)
-}
+var (
+	_ QuerierStmt = (*StmtConn)(nil)
+	_ QuerierStmt = (*StmtTx)(nil)
+	_ QuerierStmt = (*StmtPoolTx)(nil)
+)
